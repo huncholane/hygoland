a7c99369be8ff075bc9a1844d4b3c15f9886a70f
huncholane <huncho@hygo.ai>
2025-05-16 17:38:29 -0700

    add line number to read only

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.o.readonly then
   vim.keymap.set("n", "q", "<cmd>qa!<CR>", { noremap = true, silent = true })
+  vim.o.number = true
 else
   require("config.lazy")
 end
